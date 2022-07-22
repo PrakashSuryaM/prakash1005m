91b3b6ee7e2e7fb94f59ca2f866ca8685d46ebc7
PrakashSuryaM <prakash.mps.1005@gmail.com>
2022-07-22 11:43:27 +0530

    New Code

---
+++ src/test/java/TestNG/Priority.java
@@ -0,0 +1,19 @@
+package TestNG;
+
+public class Priority {
+
+
+	private void first() {
+		System.out.println("First");
+	}
+	
+	
+	private void middle() {
+		System.out.println("Middle");
+	}
+	
+	
+	private void end() {
+		System.out.println("End");
+	}
+}
